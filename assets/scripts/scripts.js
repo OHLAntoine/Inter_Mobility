92fe350e74f5365e20edac5f56341e2c78561db1
OHLAntoine <antoine.ohl@gmail.com>
2023-08-21 10:55:41 +0200

    Réglages couleurs et apparition du texte

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -4,12 +4,39 @@ window.addEventListener("load", (event) => {
     wordsResume.classList.add('loaded');
 })
 
-const resume = document.querySelector('.test');
+const resume = document.querySelector('.resume_content');
+const actions = document.querySelector('.actions_content');
+const experts = document.querySelector('.experts_content');
+const partners = document.querySelector('.partners_content');
 
 window.onscroll = function() {scrolling()};
 
 function scrolling() {
-    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-        resume.classList.add('loaded');
+    if (window.matchMedia("(min-width: 992px)").matches) {
+        if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+            resume.classList.add('loaded');
+        }
+        if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+            actions.classList.add('loaded');
+        }
+        if (document.body.scrollTop > 1300 || document.documentElement.scrollTop > 1300) {
+            experts.classList.add('loaded');
+        }
+        if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
+            partners.classList.add('loaded');
+        }
+    } else {
+        if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+            resume.classList.add('loaded');
+        }
+        if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+            actions.classList.add('loaded');
+        }
+        if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
+            experts.classList.add('loaded');
+        }
+        if (document.body.scrollTop > 2400 || document.documentElement.scrollTop > 2400) {
+            partners.classList.add('loaded');
+        }
     }
 }
